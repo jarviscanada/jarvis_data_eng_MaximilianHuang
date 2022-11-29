e98a617b6a24130a3343028525800405f413138c
Max Huang <maximilian.huang@pm.me>
2022-11-29 23:03:24 +0000

    Complete read/update/delete data lessons

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -4,12 +4,13 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class JDBCExecutor {
-  public static void main(String[] args) throws ClassNotFoundException {
+  public static void main(String[] args) {
     DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
         "hplussport", "postgres", "password");
     try {
       Connection connection = dcm.getConnection();
       CustomerDAO customerDAO = new CustomerDAO(connection);
+      /* Create new customer
       Customer customer = new Customer();
       customer.setFirstName("George");
       customer.setLastName("Washington");
@@ -19,8 +20,37 @@ public class JDBCExecutor {
       customer.setCity("Mount Vernon");
       customer.setState("VA");
       customer.setZipCode("22121");
-
       customerDAO.create(customer);
+      */ /* Read customer by ID
+      Customer customer = customerDAO.findById(1000);
+      System.out.println(customer.getFirstName() + " " + customer.getLastName());
+      */ /* Update customer
+      Customer customer = customerDAO.findById(10000);
+      System.out.println(customer.getFirstName() + " " + customer.getLastName() + " "
+          + customer.getEmail());
+      customer.setEmail("gwashington@wh.gov");
+      customer = customerDAO.update(customer);
+      System.out.println(customer.getFirstName() + " " + customer.getLastName() + " "
+          + customer.getEmail());
+      */
+      Customer customer = new Customer();
+      customer.setFirstName("John");
+      customer.setLastName("Adams");
+      customer.setEmail("jadams.wh.gov");
+      customer.setAddress("1234 Main St");
+      customer.setCity("Arlington");
+      customer.setState("VA");
+      customer.setPhone("(555) 555-9845");
+      customer.setZipCode("01234");
+
+      Customer dbCustomer = customerDAO.create(customer);
+      System.out.println(dbCustomer);
+      dbCustomer = customerDAO.findById(dbCustomer.getId());
+      System.out.println(dbCustomer);
+      dbCustomer.setEmail("john.adams@wh.gov");
+      dbCustomer = customerDAO.update(dbCustomer);
+      System.out.println(dbCustomer);
+      customerDAO.delete(dbCustomer.getId());
     } catch (SQLException e) {
       e.printStackTrace();
     }
