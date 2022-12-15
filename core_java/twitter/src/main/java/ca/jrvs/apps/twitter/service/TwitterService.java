217b8cdbefca7f78145324f4a3f2b364ee57ef8e
Max Huang <maximilian.huang@pm.me>
2022-12-15 21:27:03 +0000

    Implement Twitter service

---
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/TwitterService.java
@@ -0,0 +1,69 @@
+package ca.jrvs.apps.twitter.service;
+
+import ca.jrvs.apps.twitter.dao.CrdDao;
+import ca.jrvs.apps.twitter.model.Tweet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwitterService implements Service {
+
+  private final CrdDao dao;
+
+  //@Autowired
+  public TwitterService(CrdDao dao) {
+    this.dao = dao;
+  }
+
+  @Override
+  public Tweet postTweet(Tweet tweet) {
+    validatePostTweet(tweet);
+    return (Tweet) dao.create(tweet);
+  }
+
+  private void validatePostTweet(Tweet tweet) {
+    String text = tweet.getText();
+    Double lat = tweet.getCoordinates().getLatitude();
+    Double lon = tweet.getCoordinates().getLongitude();
+
+    if (text.length() == 0) {
+      throw new IllegalArgumentException("Tweet text cannot be empty");
+    }
+
+    if (text.length() > 140) {
+      throw new IllegalArgumentException("Tweet text exceeds 140 characters");
+    }
+
+    if ((lat > 90.0) || (lat < -90.0)) {
+      throw new IllegalArgumentException("Latitude out of bounds (range of -90.0 to 90.0)");
+    }
+
+    if ((lon > 180.0) || (lon < -180.0)) {
+      throw new IllegalArgumentException("Longitude out of bounds (range of -180.0 to 180.0)");
+    }
+  }
+
+  @Override
+  public Tweet showTweet(String id, String[] fields) {
+    validateId(id);
+    return (Tweet) dao.findById(id);
+  }
+
+
+  @Override
+  public List<Tweet> deleteTweets(String[] ids) {
+    List<Tweet> tweets = new ArrayList<>();
+    for (String id : ids) {
+      validateId(id);
+      tweets.add((Tweet) dao.deleteById(id));
+    }
+    return tweets;
+  }
+
+  private void validateId (String id) {
+    try {
+      Long.parseLong(id);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException(id + " not a valid tweet ID, must be long int");
+    }
+  }
+}
