ece8b2a1e22754a4e0e51c51bbe7d494abd09a04
Max Huang <maximilian.huang@pm.me>
2022-12-15 21:10:33 +0000

    Restructure DAO integration test to remove redundancy

--- core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
+++ core_java/twitter/src/test/java/ca/jrvs/apps/twitter/dao/TwitterDaoIntTest.java
@@ -17,6 +17,13 @@ import org.junit.Test;
 public class TwitterDaoIntTest {
 
   private TwitterDao dao;
+  private Tweet inputTweet;
+  private String hashtag;
+  private String text;
+  private Double lon;
+  private Double lat;
+
+
   private static String consumerKey;
   private static String consumerSecret;
   private static String accessToken;
@@ -29,69 +36,56 @@ public class TwitterDaoIntTest {
     accessToken = System.getenv("accessToken");
     tokenSecret = System.getenv("tokenSecret");
     System.out.println(consumerKey + "\n" + consumerSecret + "\n" + accessToken
-        + " " + tokenSecret);
+        + "\n" + tokenSecret);
   }
 
   @Before
-  public void setupTest() {
+  public void setupTest() throws JsonProcessingException {
     HttpHelper httpHelper = new TwitterHttpHelper(consumerKey, consumerSecret,
         accessToken, tokenSecret);
     dao = new TwitterDao(httpHelper);
+    hashtag = "#abc";
+    text = "@huangm66_p41 hello world " + hashtag + " " + System.currentTimeMillis();
+    lat = 1d;
+    lon = -1d;
+    inputTweet = TweetUtil.buildTweet(text, lon, lat);
+    System.out.println(JsonUtil.toPrettyJson(inputTweet, false));
   }
 
   @Test
-  public void testCreate() throws JsonProcessingException {
-    String hashtag = "#abc";
-    String text = "@huangm66_p41 hello world " + hashtag + " " + System.currentTimeMillis();
-    Double lat = 1d;
-    Double lon = -1d;
-    Tweet postTweet = TweetUtil.buildTweet(text, lon, lat);
-    System.out.println(JsonUtil.toPrettyJson(postTweet, false));
+  public void testCreate() {
+    Tweet resultTweet = dao.create(inputTweet);
 
-    Tweet tweet = dao.create(postTweet);
+    assertEquals(text, resultTweet.getText());
 
-    assertEquals(text, tweet.getText());
+    assertNotNull(resultTweet.getCoordinates());
+    assertEquals(2, resultTweet.getCoordinates().getCoordinates().size());
+    assertEquals(lon, resultTweet.getCoordinates().getLongitude());
+    assertEquals(lat, resultTweet.getCoordinates().getLatitude());
 
-    assertNotNull(tweet.getCoordinates());
-    assertEquals(2, tweet.getCoordinates().getCoordinates().size());
-    assertEquals(lon, tweet.getCoordinates().getLongitude());
-    assertEquals(lat, tweet.getCoordinates().getLatitude());
-
-    assertTrue(hashtag.contains(tweet.getEntities().getHashtags().get(0).getText()));
+    assertTrue(hashtag.contains(resultTweet.getEntities().getHashtags().get(0).getText()));
   }
 
   @Test
-  public void testFindById() throws JsonProcessingException {
-    String hashtag = "#abc";
-    String text = "@huangm66_p41 hello world " + hashtag + " " + System.currentTimeMillis();
-    Double lat = 1d;
-    Double lon = -1d;
-    Tweet postTweet = TweetUtil.buildTweet(text, lon, lat);
-    System.out.println(JsonUtil.toPrettyJson(postTweet, false));
-
-    Tweet tweet = dao.create(postTweet);
-    String idStr = tweet.getIdStr();
+  public void testFindById() {
+    Tweet resultTweet = dao.create(inputTweet);
+    String idStr = resultTweet.getIdStr();
 
     Tweet foundTweet = dao.findById(idStr);
+
     assertEquals(foundTweet.getIdStr(), idStr);
     assertEquals(foundTweet.getText(), text);
   }
 
 
   @Test
-  public void testDeleteById() throws JsonProcessingException {
-    String hashtag = "#abc";
-    String text = "@huangm66_p41 hello world " + hashtag + " " + System.currentTimeMillis();
-    Double lat = 1d;
-    Double lon = -1d;
-    Tweet postTweet = TweetUtil.buildTweet(text, lon, lat);
-    System.out.println(JsonUtil.toPrettyJson(postTweet, false));
-
-    Tweet tweet = dao.create(postTweet);
-    String idStr = tweet.getIdStr();
+  public void testDeleteById() {
+    Tweet resultTweet = dao.create(inputTweet);
+    String idStr = resultTweet.getIdStr();
 
     Tweet deletedTweet = dao.deleteById(idStr);
+
     assertEquals(deletedTweet.getIdStr(), idStr);
-    assertEquals(deletedTweet.getText(), tweet.getText());
+    assertEquals(deletedTweet.getText(), text);
   }
 }
