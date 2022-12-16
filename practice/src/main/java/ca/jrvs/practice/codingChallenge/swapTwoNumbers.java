c022552ff3f8ce99b1446a44309c9d4cdf8a6e44
Max Huang <maximilian.huang@pm.me>
2022-12-16 16:24:44 +0000

    Complete Swap Two Numbers coding challenge

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/swapTwoNumbers.java
@@ -0,0 +1,36 @@
+package ca.jrvs.practice.codingChallenge;
+
+/*
+ * https://www.notion.so/jarvisdev/Swap-two-numbers-50a6bcaa3cc14bbbb0e94fc64cc0573d
+ */
+public class swapTwoNumbers {
+  // Swap two numbers without using a third variable
+
+  /*
+   * Big-O: O(1)
+   * Only bitwise operations
+   */
+  private static void swapTwoNumbersBit(int i, int j) {
+    i = i ^ j;
+    j = j ^ i;
+    i = i ^ j;
+    System.out.println("Values now " + i + " and " + j);
+  }
+
+  /*
+   * Big-O: O(1)
+   * Only arithmetic operations
+   */
+  private static void swapTwoNumbersArithmetic(int i, int j) {
+    i += j;
+    j = i - j;
+    i -= j;
+    System.out.println("Values now " + i + " and " + j);
+  }
+
+  public static void main(String[] args) {
+    swapTwoNumbersBit(1, 2);
+    swapTwoNumbersArithmetic(2, 3);
+  }
+
+}
